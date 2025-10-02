a723a838112365aa9e272166ec8086be0d2b30b0
Nodii <13803869+NodeDiver@users.noreply.github.com>
2025-10-02 03:22:22 -0300

    Revert "feat: center support button on the same line as Pro Mode and Historical Data toggles"
    This reverts commit 95c4edc5d2b9093f5cce78d554aee51ca55c38a3.

--- pages/index.tsx
+++ pages/index.tsx
@@ -480,7 +480,7 @@ export default function Home() {
         </div>
 
         {/* Action Buttons */}
-        <div className="mt-6 flex justify-center items-center">
+        <div className="mt-6 flex justify-between items-center">
           <div className="flex items-center space-x-4">
             {isHydrated && proMode && !loading && (
               <button
@@ -556,11 +556,10 @@ export default function Home() {
                 />
               </button>
             </div>
-          </div>
-          
-          {/* Support Button - Centered */}
-          <div className="flex items-center">
-            <button
+            
+            {/* Support Button */}
+            <div className="flex items-center">
+              <button
                 onClick={async () => {
                   try {
                     // Check if WebLN is available
@@ -623,6 +622,7 @@ export default function Home() {
               >
                 Support my work with 1€
               </button>
+            </div>
           </div>
         </div>
 
